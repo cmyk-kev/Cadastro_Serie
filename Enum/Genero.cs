7688028c78396dbb385764e8f021748b06e03bfb
Kevin da Silva Felix <kevinsfelix@outlook.com>
2022-01-16 21:35:53 -0300

    Enviando o projeto

---
+++ Enum/Genero.cs
@@ -0,0 +1,20 @@
+namespace App.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        Ficacao_Cientifica = 9,
+        Musical = 10,
+        Activator = 11,
+        Suspence = 12,
+        Terror = 13
+        
+    }
+}
